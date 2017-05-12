6f2c06922536317f2a39687db0d27d0b2641cfd2
laseq <lourenco.sequeira@outlook.com>
2017-05-12 16:56:59 +0100

    Created Group model

---
+++ models/group.js
@@ -0,0 +1,20 @@
+const mongoose  = require('mongoose');
+
+const groupSchema = new mongoose.Schema({
+  name: { type: String, unique: true, trim: true, required: true },
+  members: { type: mongoose.Schema.ObjectId, ref: 'User'},
+  schedule: [{
+    day: { type: String, required: true },
+    date: {type: Date, required: true },
+    startTime: { type: Date, required: true },
+    location: { type: String, required: true },
+    distance: { type: String, required: true },
+    description: { type: String }
+  }],
+  comments: [{
+    comment: { type: String, required: true },
+    user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+  }]
+});
+
+module.exports = mongoose.model('Group', groupSchema);
