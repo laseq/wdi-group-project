53e13364e9094caaa3ec441a45ecfc18bbf52f89
laseq <lourenco.sequeira@outlook.com>
2017-05-12 16:21:17 +0100

    Created the User model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose  = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, trim: true, required: true },
+  name: { type: String, trim: true },
+  email: { type: String, unique: true, trim: true, required: true },
+  age: { type: Number, required: true },
+  gender: { type: String, enum: ['male', 'female'] },
+  image: { type: String, trim: true },
+  location: { type: String, required: true },
+  postcode: { type: String, required: true },
+  locationCoords: [{ lat: { type: Number }, lng: { type: Number } }],
+  about: { type: String },
+  groups: [{ type: mongoose.Schema.ObjectId, ref: 'Group'}]
+});
+
+module.exports = mongoose.model('User', userSchema);
