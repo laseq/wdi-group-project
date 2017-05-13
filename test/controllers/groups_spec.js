453b277bf01fe72375af48396901e60da8a392c3
laseq <lourenco.sequeira@outlook.com>
2017-05-13 11:14:04 +0100

    Continued with TDD set up

--- test/controllers/groups_spec.js
+++ test/controllers/groups_spec.js
@@ -1,14 +1,22 @@
-require('../spec_helper');
+const { api, expect } = require('../spec_helper');
 
 describe('Groups Controller Test', () => {
 
   describe('GET /api/groups', () => {
     it('should return a 200 response', done => {
-      done();
-    });
+      api
+        .get('/api/groups')
+        .set('Accept', 'application/json')
+        .end((err,res) => {
+          if (err) console.log(err);
+          expect(res.status).to.be.eq(200);
+          done();
+        });
+    }); // End of it('should return a 200 response'...)
   }); // End of describe('GET /api/groups'...)
 
 
-  
+
+
 
 }); // End of describe('Groups Controller Test'...)
