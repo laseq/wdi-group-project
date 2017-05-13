04ec0c40e31a0c2ee188722ab44535f8e4571096
laseq <lourenco.sequeira@outlook.com>
2017-05-13 16:48:06 +0100

    Merging and committing for the end of the day

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,1 @@
-const router = require('express').Router();
-const groups = require('../controllers/groups');
 
-router.route('/groups')
-  .get(groups.index)
-  .post(groups.create);
-
-router.route('/groups/:id')
-  .get(groups.show);
-
-module.exports = router;
