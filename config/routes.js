047814c7a3b3bed005940b8579e943c3a33feca4
AlexYeates <AlexYeates@hotmail.com>
2017-05-13 16:48:50 +0100

    Added Lourenco routes

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const users  = require('../controllers/users');
+const groups = require('../controllers/groups');
 
 router.route('/users')
 .get(users.index)
@@ -9,5 +10,13 @@ router.route('/users/:id')
 .get(users.show)
 .put(users.update)
 .delete(users.delete);
+
+router.route('/groups')
+.get(groups.index)
+.post(groups.create);
+
+router.route('/groups/:id')
+.get(groups.show);
+
 
 module.exports = router;
