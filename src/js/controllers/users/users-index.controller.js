920795b63d6588ee87d526dffea51df3e646d2e8
laseq <lourenco.sequeira@outlook.com>
2017-05-14 20:22:14 +0100

    We can now send a token with the Authorization header to access secure web pages

---
+++ src/js/controllers/users/users-index.controller.js
@@ -0,0 +1,9 @@
+angular
+  .module('runchApp')
+  .controller('UsersIndexCtrl', UsersIndexCtrl);
+
+UsersIndexCtrl.$inject = ['User'];
+function UsersIndexCtrl(User) {
+  const vm = this;
+  vm.users = User.query();
+}
