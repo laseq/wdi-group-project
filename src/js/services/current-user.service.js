0f23054a04ecc15adfeab53c47265006e217f8f8
laseq <lourenco.sequeira@outlook.com>
2017-05-14 20:41:46 +0100

    Created CurrentUserService

---
+++ src/js/services/current-user.service.js
@@ -0,0 +1,25 @@
+angular
+  .module('runchApp')
+  .service('CurrentUserService', CurrentUserService);
+
+CurrentUserService.$inject = ['TokenService', '$rootScope', 'User'];
+function CurrentUserService(TokenService, $rootScope, User) {
+  const self = this;
+
+  self.getUser = () => {
+    const decoded = TokenService.decodeToken();
+    console.log('decoded:', decoded);
+
+    if (decoded) {
+      User
+        .get({ id: decoded.id })
+        .$promise
+        .then(data => {
+          console.log('decoded data:', data);
+        })
+        .catch(err => {
+          console.log('decoded error:', err);
+        });
+    }
+  };
+}
