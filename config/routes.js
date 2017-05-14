0593d793d3aa7d296f1ede336b158baca974d04f
AlexYeates <AlexYeates@hotmail.com>
2017-05-14 13:55:13 +0100

    Added authentications to config routes

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,12 @@
-const router = require('express').Router();
-const users  = require('../controllers/users');
-const groups = require('../controllers/groups');
+const router          = require('express').Router();
+const users           = require('../controllers/users');
+const groups          = require('../controllers/groups');
+const authentications = require('../controllers/authentications');
+
+router.route('/register')
+.post(authentications.register);
+router.route('/login')
+.post(authentications.login);
 
 router.route('/users')
 .get(users.index)
