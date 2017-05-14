f769c3d489af56313c8ddd79069135b49a959273
laseq <lourenco.sequeira@outlook.com>
2017-05-14 19:08:52 +0100

    Let the AuthInterceptor take care of setting the token in local storage rather than the login and register controllers

--- src/js/factories/auth-interceptor.factory.js
+++ src/js/factories/auth-interceptor.factory.js
@@ -2,15 +2,18 @@ angular
   .module('runchApp')
   .factory('AuthInterceptor', AuthInterceptor);
 
-AuthInterceptor.$inject = [];
-function AuthInterceptor() {
+AuthInterceptor.$inject = ['API', 'TokenService'];
+function AuthInterceptor(API, TokenService) {
   return {
     request(config) {
       console.log('config:', config);
       return config;
     },
     response(res) {
-      console.log('res:', res);
+      if (res.config.url.indexOf(API) === 0 && res.data.token) {
+        TokenService.setToken(res.data.token);
+      }
+
       return res;
     }
   };
