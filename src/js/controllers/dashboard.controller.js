ea9484b7c6ed707697c63bf3ee760400d9326eb9
AlexYeates <AlexYeates@hotmail.com>
2017-05-15 23:06:47 +0100

    Can't access the array within the groups array returned after a query.

--- src/js/controllers/dashboard.controller.js
+++ src/js/controllers/dashboard.controller.js
@@ -11,18 +11,40 @@ function DashboardCtrl(Group, $stateParams, $state, CurrentUserService) {
   const vm = this;
   vm.user = CurrentUserService.currentUser.id;
 
+
   const usersGroups = [];
 
   memberOf();
 
   function memberOf() {
-    const group = vm.all
+    const group = vm.all;
     vm.all = Group.query()
     .$promise
     .then((groups) => {
-    // for each to check if the current user id matches one of a groups members, if so, push the group to usersGroup array
+      // for each to check if the current user id matches one of a groups members, if so, push the group to usersGroup array
+      console.log(vm.user);
+      for (var i = 0; i < groups.length; i++) {
+        console.log(groups);
+      }
+        // for (var j = 0; j < groups[i].length; j++) {
+        //   console.log();
+        // }
 
-      console.log(groups[0].members);
+        // for (var j = 0; j < groups.length; j++) {
+        //   if (groups.includes(vm.user))
+        //     console.log('yes');
+        //     // then push the group's name to the userGroups array
+        // }
+
+        // for(var i = 0; i < parentArray.length; i++){
+        //   for(var j = 0; j < parentArray[i].length; j++){
+        //
+        //     console.log(parentArray[i][j]);
+        //   }
+        // }
+      
+
+      // console.log(groups);
     });
   }
 }
