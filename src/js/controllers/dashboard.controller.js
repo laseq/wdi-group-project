41470fd3725e5a1dc172a6aa587e50163a2a6e3b
AlexYeates <AlexYeates@hotmail.com>
2017-05-15 23:17:10 +0100

    Logging out members of each group in an array

--- src/js/controllers/dashboard.controller.js
+++ src/js/controllers/dashboard.controller.js
@@ -2,16 +2,12 @@ angular
 .module('runchApp')
 .controller('DashboardCtrl', DashboardCtrl);
 
-
 // show the user's groups by gettig the current users id, and seeing if it matches any of those in a group's members array?
-
 DashboardCtrl.$inject = ['Group', '$stateParams', '$state', 'CurrentUserService'];
 
 function DashboardCtrl(Group, $stateParams, $state, CurrentUserService) {
   const vm = this;
   vm.user = CurrentUserService.currentUser.id;
-
-
   const usersGroups = [];
 
   memberOf();
@@ -23,8 +19,8 @@ function DashboardCtrl(Group, $stateParams, $state, CurrentUserService) {
     .then((groups) => {
       // for each to check if the current user id matches one of a groups members, if so, push the group to usersGroup array
       console.log(vm.user);
-      for (var i = 0; i < groups[i].members.length; i++) {
-        console.log(groups[i]); // logs out objects of the groups
+      for (var i = 0; i < groups.length; i++) {
+        console.log(groups[i].members); // logs out objects of the groups
       }
     });
   }
