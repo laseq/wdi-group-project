642d40c23f7b18231df582a607c6bec6c7dd7ecd
laseq <lourenco.sequeira@outlook.com>
2017-05-15 17:15:06 +0100

    Displayed the names of the commenters from their user ids

--- src/js/controllers/groups/groups-show.controller.js
+++ src/js/controllers/groups/groups-show.controller.js
@@ -10,6 +10,8 @@ function GroupsShowCtrl(Group, $stateParams, TokenService, $state, User) {
   vm.currentUserId = TokenService.decodeToken().id;
   vm.delete = groupsDelete;
   vm.memberArray = [];
+  vm.commenters = [];
+
   getGroupDetails();
 
   function getGroupDetails() {
@@ -20,6 +22,7 @@ function GroupsShowCtrl(Group, $stateParams, TokenService, $state, User) {
         vm.group = group;
         getAdminDetails();
         getMemberDetails();
+        getCommenters();
       })
       .catch(err => console.log('error in getGroupDetails:', err));
   }
@@ -52,6 +55,30 @@ function GroupsShowCtrl(Group, $stateParams, TokenService, $state, User) {
         })
         .catch(err => console.log('error in getMemberDetails:', err));
     });
+  }
+
+  function getCommenters() {
+    // vm.group.comments.forEach(comment => {
+    //   User
+    //     .get({ id: comment.user })
+    //     .$promise
+    //     .then(commenter => {
+    //       vm.commenters.push(commenter);
+    //     })
+    //     .catch(err => console.log('error in getCommenters:', err));
+    // });
+
+    for (let i=0; i<vm.group.comments.length; i++) {
+      User
+        .get({ id: vm.group.comments[i].user })
+        .$promise
+        .then(commenter => {
+          vm.group.comments[i].userDetails = commenter;
+        })
+        .catch(err => console.log('error in getCommenters:', err));
+    }
+
+
   }
 
 }
