bf1c839f8b5b5534be0ec70213a3274e95ba56f2
AlexYeates <AlexYeates@hotmail.com>
2017-05-16 12:30:25 +0100

    Commented out a function that was conflicting

--- src/js/controllers/dashboard.controller.js
+++ src/js/controllers/dashboard.controller.js
@@ -9,30 +9,32 @@ function DashboardCtrl(Group, $stateParams, $state, CurrentUserService) {
   vm.loggedInUser = CurrentUserService.currentUser;
   vm.ownedGroups  = [];
 
+  // groupsIndex();
   getGroupsAndFindYourGroups();
 
-  function groupsIndex() {
-    vm.all = Group.query();
-  }
+
+  // function groupsIndex() {
+  //   vm.all = Group.query();
+  // }
 
   function getGroupsAndFindYourGroups() {
     Group
-      .query()
-      .$promise
-      .then(groups => {
-        vm.all = groups;
-        console.log('vm.all:', vm.all);
-        groups.forEach(group => {
-          console.log('group:', group);
-          console.log('vm.loggedInUser._id:', vm.loggedInUser._id);
-          console.log('group.admin.id:', group.admin.id);
-          if(vm.loggedInUser._id === group.admin.id){
-            console.log(`${vm.loggedInUser.username} is the admin of ${group.name}`);
-            vm.ownedGroups.push(group);
-          }
-        });
-      })
-      .catch(err => console.log(err));
+    .query()
+    .$promise
+    .then(groups => {
+      vm.all = groups;
+      console.log('vm.all:', vm.all);
+      groups.forEach(group => {
+        console.log('group:', group);
+        console.log('vm.loggedInUser._id:', vm.loggedInUser._id);
+        console.log('group.admin.id:', group.admin.id);
+        if(vm.loggedInUser._id === group.admin.id){
+          console.log(`${vm.loggedInUser.username} is the admin of ${group.name}`);
+          vm.ownedGroups.push(group);
+        }
+      });
+    })
+    .catch(err => console.log(err));
   }
 
   function createdGroup() {
@@ -41,7 +43,5 @@ function DashboardCtrl(Group, $stateParams, $state, CurrentUserService) {
     vm.all.forEach(group => {
       console.log('group:', group);
     });
-
-
   }
 }
